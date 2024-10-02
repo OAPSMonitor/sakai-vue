9facdb612633c1b4236c437a230b4fbf4528eb4d
Eric Roué <eric.roue@kyndryl.com>
2024-10-02 22:21:42 +0200

    Début de migration vers Typescript + Création du composant MyFirstComponent

---
+++ src/components/tests/MyFirstComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <Button @click="updateMessage">Update Message</Button>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import Button from 'primevue/button';
+import { ref } from 'vue';
+
+const message = ref('Hello, world!');
+
+const updateMessage = () => {
+    message.value = 'Hello, Vue 3 with TypeScript!';
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
--- src/views/pages/Empty.vue
+++ src/views/pages/Empty.vue
@@ -2,5 +2,10 @@
     <div className="card">
         <div class="font-semibold text-xl mb-4">Empty Page</div>
         <p>Use this page to start from scratch and place your custom content.</p>
+        <MyFirstComponent />
     </div>
 </template>
+
+<script setup lang="ts">
+import MyFirstComponent from '@/components/tests/MyFirstComponent.vue';
+</script>
